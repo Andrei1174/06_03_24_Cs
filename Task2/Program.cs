fd829908533a4ca77b13c123ccafc5bb740ec53c
Andrei1174 <aremin1174@gmail.com>
2024-03-06 15:06:05 +0900

    added fail Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,20 +1,27 @@
 ﻿
-int num1 = 5;
+int num1 = 10;
 int num2 = 6;
-int sum = num1 + num2;
-System.Console.WriteLine(sum);
+// int sum = num1 + num2;
+// System.Console.WriteLine(sum);
 
 long l1 = 6;
-long sum2 = l1 + num1;
-Console.WriteLine(sum2);
+// long sum2 = l1 + num1;
+// Console.WriteLine(sum2);
 
-double d1 = 2.3;
-double sum3 = d1 + num1;
-Console.WriteLine(sum3);
+ double d1 = 2.3;
+// double sum3 = d1 + num1;
+// Console.WriteLine(sum3);
 
-int produkt1 = num1 * num2;
-long produkt2 = l1 * num1;
-double produkt3 = d1 + num1;
-Console.WriteLine(produkt1);
-Console.WriteLine(produkt2);
-Console.WriteLine(produkt3);
+// int produkt1 = num1 * num2;
+// long produkt2 = l1 * num1;
+// double produkt3 = d1 + num1;
+// Console.WriteLine(produkt1);
+// Console.WriteLine(produkt2);
+// Console.WriteLine(produkt3);
+
+
+int guolient = num1 / num2;
+Console.WriteLine(guolient);
+
+double guolient1 = (double) num1 / num2;
+Console.WriteLine(guolient1);
