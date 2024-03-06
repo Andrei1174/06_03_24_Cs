ff6936757adea0646b5a9d001509bffa91c17a3c
Andrei1174 <aremin1174@gmail.com>
2024-03-06 16:46:45 +0900

    added fail Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,11 +4,11 @@ int num2 = 6;
 // int sum = num1 + num2;
 // System.Console.WriteLine(sum);
 
-long l1 = 6;
+//long l1 = 6;
 // long sum2 = l1 + num1;
 // Console.WriteLine(sum2);
 
- double d1 = 2.3;
+ //double d1 = 2.3;
 // double sum3 = d1 + num1;
 // Console.WriteLine(sum3);
 
