1de44ecbfcf3feb74d8829b58785bfd57f1b0cd3
Andrei1174 <aremin1174@gmail.com>
2024-03-06 16:29:11 +0900

    added fail Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int num1 = 99;
+int num2 = 8;
+int num3 = 18;
+int num4 = 813;
+int num5 = 56;
+int max = num1;
+
+if (max < num2) max = num2;
+else if (max < num3) max = num3;
+else if (max < num4) max = num4;
+else if (max < num5) max = num5;
+Console.WriteLine(max);
+
+    
+
+
+
+
