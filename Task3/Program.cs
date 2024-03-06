7a8fcf9e02120b0475ca40cdf973635ca275160c
Andrei1174 <aremin1174@gmail.com>
2024-03-06 16:16:24 +0900

    added fail Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+
+
+int num1 = 9;
+int num2 = 9;
+if(num1 == num2) Console.WriteLine($"{num1} равно {num2}");
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} больше {num2}");
+}
+if(num1 < num2)
+{
+    Console.WriteLine($"{num2} больше {num1}");
+}
