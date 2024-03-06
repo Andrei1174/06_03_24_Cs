16eeaf6f237bdeb0da83e2744542c9f68a7be4af
Andrei1174 <aremin1174@gmail.com>
2024-03-06 14:55:43 +0900

    added fail Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int num1 = 5;
+int num2 = 6;
+int sum = num1 + num2;
+System.Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + num1;
+Console.WriteLine(sum2);
+
+double d1 = 2.3;
+double sum3 = d1 + num1;
+Console.WriteLine(sum3);
+
+int produkt1 = num1 * num2;
+long produkt2 = l1 * num1;
+double produkt3 = d1 + num1;
+Console.WriteLine(produkt1);
+Console.WriteLine(produkt2);
+Console.WriteLine(produkt3);
