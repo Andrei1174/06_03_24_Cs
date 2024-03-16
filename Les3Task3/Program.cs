c515f1e7342f532f4322e81b537ba7026113af72
Andrei1174 <aremin1174@gmail.com>
2024-03-16 19:50:00 +0900

    added fail Les3Task3

--- Les3Task3/Program.cs
+++ Les3Task3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]+" ");
+    }
+
+    // foreach (var item in arr)  // // можно так 
+    // {
+    //     Console.Write(item+" ");
+    // }
+}
+
+int[] mas = [1, 2, 3, 4, 5, 6];
+ZeroEvenElements(mas);
+PrintArray(mas);
+
