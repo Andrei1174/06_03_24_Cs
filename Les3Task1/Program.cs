1547ffd5bd5f309d8bda2ab689cb67e8420943d8
Andrei1174 <aremin1174@gmail.com>
2024-03-16 18:23:37 +0900

    added fail Les3Task1

--- Les3Task1/Program.cs
+++ Les3Task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numerator = a * b;
+    int denomenator = c + d;
+    double rez = numerator / denomenator;
+    return rez;
+}
+
+double rezult = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(rezult);
