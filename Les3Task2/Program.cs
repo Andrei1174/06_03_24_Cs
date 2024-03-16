25ce50c1960a8f5cf0169fe04ab8f8b6fc1c3240
Andrei1174 <aremin1174@gmail.com>
2024-03-16 19:07:17 +0900

    added fail Les3Task2

--- Les3Task2/Program.cs
+++ Les3Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static void PrintSguares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.WriteLine($"{i} => {i*i} ");
+        i++;
+    }
+}
+// PrintSguares(5);
+// PrintSguares(10);
+// PrintSguares(15);
+PrintSguares(20);
+
