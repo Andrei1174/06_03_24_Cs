5c2d89ab9b2475e0b564ee70f54faf0372ba25bd
Andrei1174 <aremin1174@gmail.com>
2024-03-20 19:22:32 +0900

    added fail Les4Task1

---
+++ Les4Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int[,] CreateMatrix(int row, int colums ,int min,int max)
+{
+    int[,] matrix = new int[row, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix2D = CreateMatrix(4,3,1,100);
+PrintMatrix(matrix2D);
